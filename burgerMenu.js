2aa2411e76b084f0c06b3811520d6781a8d403ba
Yevhenii Herasymenko <74263798+Zhekager@users.noreply.github.com>
2022-07-12 00:39:46 +0300

    bag-fix styled

--- burgerMenu.js
+++ burgerMenu.js
@@ -1,27 +1,27 @@
 const ref = {
-  burgerMenu: document.querySelector(".nav-menu_icon"),
-  modalMenu: document.querySelector(".modal-menu"),
-  closeMenu: document.querySelector(".modal_btn-close"),
+  burgerMenu: document.querySelector('.nav-menu_icon'),
+  modalMenu: document.querySelector('.modal-menu'),
+  closeMenu: document.querySelector('.modal_btn-close'),
 };
 
-ref.burgerMenu.addEventListener("click", openMenu);
-ref.closeMenu.addEventListener("click", closeMenu);
-window.addEventListener("keydown", keyCloseMenu);
+ref.burgerMenu.addEventListener('click', openMenu);
+ref.closeMenu.addEventListener('click', closeMenu);
+window.addEventListener('keydown', keyCloseMenu);
 
 function openMenu() {
-  ref.modalMenu.classList.remove("is-hidden");
+  ref.modalMenu.classList.toggle('is-hidden');
 }
 
 function closeMenu(evt) {
-  const contains = ref.modalMenu.classList.contains("is-hidden");
+  const contains = ref.modalMenu.classList.contains('is-hidden');
 
   {
-    !contains && ref.modalMenu.classList.add("is-hidden");
+    !contains && ref.modalMenu.classList.add('is-hidden');
   }
 }
 
 function keyCloseMenu(evt) {
-  if (evt.code === "Escape") {
-    ref.modalMenu.classList.add("is-hidden");
+  if (evt.code === 'Escape') {
+    ref.modalMenu.classList.add('is-hidden');
   }
 }
